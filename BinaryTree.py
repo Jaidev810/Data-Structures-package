fd61e59328f0667b465eb110c9d253dcb0fd3096
jaidev <jaidevchaudhary810@gmail.com>
2021-02-04 23:14:59 +0530

    SinglyLinked List done

---
+++ BinaryTree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init(self, val: int):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def takeinput(self):
+        pass
